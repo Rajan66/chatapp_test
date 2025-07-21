d4531369807532402713a166333495c67777e9d2
Rajan66 <rajanmaharjan042@gmail.com>
2025-07-21 22:32:32 +0545

    feat: async chat app

--- webapp/core/settings.py
+++ webapp/core/settings.py
@@ -59,6 +59,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 ASGI_APPLICATION = "core.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6378)],
+        },
+    },
+}
 
 
 # Database
